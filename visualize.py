8f6868bdccffad559fae36a77b29f7b23a6186e2
Nairit Banerjee <nairit32@gmail.com>
2017-12-19 12:02:56 +0530

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,4 +1,22 @@
+#Import required libraries
 import pandas as pd
-from matplotliv import pyplot as plt
+from matplotlib import pyplot as plt
 %matplotlib inline
 import seaborn as sns
+
+#Read data
+df = pd.read_csv('F:\Pokemon.csv', index_col=0)
+
+#To plot a scatter plot
+sns.lmplot(x='Attack', y='Defense', data=df)
+#Can also be written as sns.lmplot(x=df.attack,y=df.Defense)
+
+#Customize using seaborn and matplotlib
+sns.lmplot(x='Attack', y='Defense', data=df, fit_reg=False, hue='Stage')
+plt.xlim(0,None)
+plt.ylim(0,None)
+
+#Box plot only the stats
+stats_df=df.drop(['Total', 'Stage', 'Legendary'], axis=1)
+sns.boxplot(data=stats_df)
+
